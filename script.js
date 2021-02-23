40472e4d2b241dc60238ac200d8b0339e0a5d850
Michael <antenehnigussie1@yahoo.com>
2021-02-23 17:42:00 +0000

    Completed Level 400 of the project. Updated files to meet the requirements of Level 400 of the project.

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 // initial page setup
 function setup() {
-  getAllEpisodes("https://api.tvmaze.com/shows/82/episodes");
+  const allShows = getAllShows().map((show) => show.id);
+  const firstShowId = allShows[0];
+  getAllEpisodes(firstShowId);
 }
 
-function getAllEpisodes(tvShowUrl) {
-  fetch(tvShowUrl)
+function getAllEpisodes(showId) {
+  const episodesLink = `https://api.tvmaze.com/shows/${showId}/episodes`;
+  fetch(episodesLink)
     .then((response) => response.json())
-    .then((data) => makePageForEpisodes(data))
-    .catch((error) => console.log(error.message));
+    // on successful response
+    .then((data) => {
+      createInitialContent(true);
+      setInitialStyling();
+      populateItems("shows", "string");
+      makePageForEpisodes(data);
+    })
+    // on failed response
+    .catch(() => createInitialContent(false));
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  const episodes = document.createElement("div"); // a div container for episode thumbnails
-  episodes.classList = "episodes";
-  // initial html content
-  rootElem.innerHTML = createInitialContent();
-  // set the page's initial css styles
-  setInitialStyling();
-
-  const select = document.getElementById("episodes"); // the <select> element
-  for (let i in episodeList) {
-    let ep = episodeList[i]; // ep = episode data
-    ep = createDisplayData(ep); // update ep with synthesised episode display data
-    select.appendChild(ep[0]); // add options to select
-    episodes.appendChild(ep[1]); // add episode div element
-  }
-  rootElem.appendChild(episodes);
-  // add event listeners to applicable html elements
+function makePageForEpisodes(episodesData) {
+  // const rootElem = document.getElementById("root");
+  const episodesList = episodesData.map((epData) => [
+    epData.name,
+    epData.season,
+    epData.number,
+  ]); // epData = fetched single episode data
+  populateItems(episodesList, "array");
+  populateItems(episodesData, "data");
   createEventListeners();
   // update miscellaneous info (e.g. number of episodes being displayed)
   updateDisplayInfo();
 }
 
-function createInitialContent() {
-  const initialContent = `
+function createInitialContent(status) {
+    const rootElem = document.getElementById("root");
+  if (status === true) {
+  rootElem.innerHTML = `
   <p class="source info">Content from site:&nbsp;<span id="source"><a href="https://tvmaze.com/" target="_blank" style="text-decoration:none; color:darkred">TVMaze.com</a></span></p>
   <form class="flex-form">
+    <select name="shows" id="shows">
+    </select>
     <select name="episodes" id="episodes">
       <option value="">All episodes</option>
     </select>
@@ -44,19 +49,59 @@ function createInitialContent() {
     <span id="btn-search"><i class="fa fa-search"></i></span>
   </form>
   <p class="result info"><span>Displaying</span>&nbsp;<span id="quantity"></span>/<span id="total"></span>&nbsp;episode(s)</p>
-  `;
-  return initialContent;
+  <div class="episodes">`;
+  } else {
+    rootElem.innerHTML = `<p class="error"><span class="red">Error!</span><br>Sorry, cannot load the requested content.<br>Please try refreshing the page.</p>`;
+  }
 }
 
 window.onload = setup;
 
+function populateItems(arg, type) {
+  switch (type) {
+    case "string":
+      const list = document.getElementById(arg); // show selector
+      if (!list.length) {
+        const options = getAllShows().map((show) => [show.name, show.id]);
+        for (let i in options) {
+          const option = document.createElement("option");
+          option.value = options[i][0];
+          option.textContent = option.value;
+          option.id = options[i][1];
+          list.appendChild(option);
+        }
+      }
+      break;
+    case "array":
+      const listArray = arg;
+      const select = document.getElementById("episodes"); // the <select> element
+      for (let i in listArray) {
+        const option = document.createElement("option"); // an <option> element (child to "select")
+        const epName = listArray[i][0]; // episode name, e.g "Winter is Coming"
+        const epSeason = listArray[i][1].toString().padStart(2, "0"); // episode season, e.g. "05"
+        const epNumber = listArray[i][2].toString().padStart(2, "0"); // episode number, e.g. "02"
+        option.value = `${epName} - S${epSeason}E${epNumber}`;
+        option.textContent = `S${epSeason}E${epNumber} - ${epName}`;
+        select.appendChild(option); // add options to select
+      }
+      break;
+    default:
+      const episodeData = arg;
+      const episodes = document.querySelector(".episodes"); // a div container for episode thumbnails
+      for (let i in episodeData) {
+        let ep = episodeData[i]; // ep = episode data
+        ep = createDisplayData(ep); // update ep with synthesised episode display data
+        episodes.appendChild(ep); // add episode div element
+      }
+      break;
+  }
+}
 function createDisplayData(episode) {
   // declare (and assigin) variables/constants related to html elements
   const thumbnail = document.createElement("div"); // a <div> element to display important episode information
   const title = document.createElement("h4"); // episode name and other important information
   const img = document.createElement("img"); // a medium-sized image representing the episode
   const summary = document.createElement("div"); // episode summary
-  const option = document.createElement("option"); // an <option> element (child to "select")
   // other constants
   const epName = episode.name; // episode name, e.g "Winter is Coming"
   const epSeason = episode.season.toString().padStart(2, "0"); // episode season, e.g. "05"
@@ -69,13 +114,10 @@ function createDisplayData(episode) {
   img.src = episode.image.original;
   summary.innerHTML = episode.summary;
   summary.classList = "summary";
-  option.value = `${epName} - S${epSeason}E${epNumber}`;
-  option.textContent = `S${epSeason}E${epNumber} - ${epName}`;
-  // add html elements to appropriate parent containers
   thumbnail.appendChild(title);
   thumbnail.appendChild(img);
   thumbnail.appendChild(summary);
-  return [option, thumbnail];
+  return thumbnail;
 }
 
 // set initial styling
@@ -89,6 +131,7 @@ function setInitialStyling() {
 
 // add event listeners
 function createEventListeners() {
+  document.getElementById("shows").addEventListener("input", selectShow);
   document.getElementById("episodes").addEventListener("input", selectEpisode);
   document
     .getElementById("txt-search")
@@ -126,6 +169,14 @@ function resetSearchBox(e) {
 function searchEpisodes(e) {
   performEpisodeAction("search", e);
   updateDisplayInfo();
+}
+
+// a function to manage selection of a tv show from list
+function selectShow(e) {
+  const selectedShow = document.getElementById(e.target.id).selectedOptions[0]
+    .id;
+  controlSearch(e);
+  getAllEpisodes(selectedShow);
 }
 
 // a function to manage selection of an episode from list
@@ -174,11 +225,22 @@ function controlSearch(event) {
   const button = document.getElementById("btn-search");
   const icon = button.querySelector(".fa");
   const input = document.getElementById("txt-search");
-  const select = document.getElementById("episodes");
-
+  const showSelector = document.getElementById("shows");
+  const episodeSelector = document.getElementById("episodes");
+  
   switch (el) {
-    case select:
-      if (select.value !== "") {
+    case showSelector:
+      const options = document.querySelectorAll("#episodes option");
+      for (let i of options) {
+        options.item(i).remove;
+      }
+      if (input.value !== "") {
+        input.value = "";
+        changeButtonStyle("search");
+      }
+      break;
+    case episodeSelector:
+      if (episodeSelector.value !== "") {
         input.value = "";
         input.hidden = true;
       }
@@ -187,9 +249,9 @@ function controlSearch(event) {
     default:
       // = button
       const totalEpisodes = Array.from(document.querySelectorAll(".thumbnail"));
-      if (select.value !== "" || input.value !== "") {
+      if (episodeSelector.value !== "" || input.value !== "") {
         totalEpisodes.forEach((episode) => showHideThumbnail(episode, true));
-        select.value = "";
+        document.getElementById("episodes").value = "";
         input.value = "";
       }
       if (icon.classList.toString().includes("fa-search")) {
